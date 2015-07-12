a3d9fb7fa50f993db139bb798f602fb548da7a95
SAURAV <sauravdash1@gmail.com>
2015-07-12 17:27:57 -0500

    Chap4-Finished

--- test/springapp/web/InventoryControllerTests.java
+++ test/springapp/web/InventoryControllerTests.java
@@ -1,6 +1,11 @@
 package springapp.web;
 
+import java.util.Map;
+
 import org.springframework.web.servlet.ModelAndView;
+
+import springapp.service.SimpleProductManager;
+import springapp.web.InventoryController;
 
 import junit.framework.TestCase;
 
@@ -8,10 +13,12 @@ public class InventoryControllerTests extends TestCase {
 
     public void testHandleRequestView() throws Exception{
         InventoryController controller = new InventoryController();
+        controller.setProductManager(new SimpleProductManager());
         ModelAndView modelAndView = controller.handleRequest(null, null);
         assertEquals("hello", modelAndView.getViewName());
         assertNotNull(modelAndView.getModel());
-        String nowValue = (String) modelAndView.getModel().get("now");
+        Map modelMap = (Map) modelAndView.getModel().get("model");
+        String nowValue = (String) modelMap.get("now");
         assertNotNull(nowValue);
     }
 }
